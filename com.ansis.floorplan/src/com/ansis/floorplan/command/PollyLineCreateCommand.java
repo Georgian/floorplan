db2839bdd3bee1a2a809aa5f233263f90124c821
norbimona <norbi0104@yahoo.com>
2013-08-09 12:04:13 +0300

    Added a createCommand for our new polyline figure.

---
+++ com.ansis.floorplan/src/com/ansis/floorplan/command/PollyLineCreateCommand.java
@@ -0,0 +1,59 @@
+package com.ansis.floorplan.command;
+
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.commands.Command;
+
+import com.ansis.floorplan.model.Canvas;
+import com.ansis.floorplan.model.PollyLine;
+
+
+public class PollyLineCreateCommand extends Command {
+
+	// ====================== 2. Instance Fields =============================
+
+	private Canvas en;
+
+	private PollyLine srv;
+
+
+	// ==================== 4. Constructors ====================
+
+	public PollyLineCreateCommand() {
+		super();
+		en = null;
+		srv = null;
+	}
+
+
+	// ==================== 7. Getters & Setters ====================
+
+	public void setPollyLine(final Object s) {
+		if (s instanceof PollyLine)
+			this.srv = (PollyLine)s;
+	}
+
+	public void setCanvas(final Object e) {
+		if (e instanceof Canvas)
+			this.en = (Canvas)e;
+	}
+
+	public void setLayout(final Rectangle r) {
+		if (srv == null)
+			return;
+		srv.setLayout(r);
+	}
+
+	@Override
+	public boolean canExecute() {
+		if (srv == null || en == null)
+			return false;
+		return true;
+	}
+
+	@Override
+	public void execute() {
+		en.addPChild(srv);
+		srv.setParent(en);
+	}
+
+}
