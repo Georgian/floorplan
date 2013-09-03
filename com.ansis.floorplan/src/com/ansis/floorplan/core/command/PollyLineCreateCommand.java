a00b049fb163f371b99cae263c7b1588ae11412d
norbimona <norbi0104@yahoo.com>
2013-09-03 12:02:03 +0300

    Fully working draw tool.

--- com.ansis.floorplan/src/com/ansis/floorplan/core/command/PollyLineCreateCommand.java
+++ com.ansis.floorplan/src/com/ansis/floorplan/core/command/PollyLineCreateCommand.java
@@ -37,166 +37,198 @@ public class PollyLineCreateCommand extends Command {
 	@SuppressWarnings("static-access")
 	@Override
 	public void execute() {
-		if (canvas.getChildren().size() == 0)
+		if (canvas.getChildren().size() == 0 && size == null)
 		{	
-			final Polly newPolly = new Polly();
+			final PollyLine newPolly = new PollyLine();
 			final PointList builderList = new PointList();
 
 			builderList.addPoint(location.x, location.y);
 			point = new Point();
-			point.x = 3;
-			point.y = 3;
-			bounds = new Rectangle(new Point(-100,-100),new Point(100,100));
+			point.x = location.x +3;
+			point.y = location.y +3;
+			bounds = new Rectangle(location, point);
 
 			newPolly.setBounds(bounds);
 			newPolly.setLayout(bounds);
-			newPolly.setLabelPosition(new Rectangle(0,0,10,10));
-
+			newPolly.setDrawingDenied(false);
 			newPolly.setList(builderList);
-			
+
 			canvas.addChild(newPolly);
-		
+
 		}
-			else if (size == null)
-			{ 	
-				if (canvas.getChildren().size() > 0)
-				{
-					final Polly newPolly = (Polly) canvas.getChildren().get(canvas.getChildren().size()-1);
-					PointList builderList = newPolly.getList();
+		else if (size == null && canvas.getChildren().size() > 0 )
+		{ 	
+			final PollyLine newPolly = (PollyLine) canvas.getChildren().get(canvas.getChildren().size()-1);
+			PointList builderList = newPolly.getList();
 
-					builderList.addPoint(location.x, location.y);
-
-					tempBounds = newPolly.getBounds();
-					helper = new Point();
-					helper.x = tempBounds.x;
-					helper.y = tempBounds.y;
+			builderList.addPoint(location.x, location.y);
 
 
-					for (int i = 0; i < builderList.size(); i++) {
-						if (minPoint == null)
-							minPoint = builderList.getPoint(i);
+			tempBounds = newPolly.getBounds();
+			System.out.println(newPolly.getBounds());
+			System.out.println("//////////");
+			helper = new Point();
 
-						if (maxPoint == null)
-							maxPoint = builderList.getPoint(i);
+			helper.x = 0;
+			helper.y = 0;
 
-						point = builderList.getPoint(i);
-
-						if(point.x < minPoint.x)
-							minPoint.x = point.x;
-						if(point.x > maxPoint.x)
-							maxPoint.x = point.x;
-						if(point.y < minPoint.y)
-							minPoint.y = point.y;
-						if(point.y > maxPoint.y)
-							maxPoint.y = point.y;
-					}
-
-					//					if(minPoint.x != 0)
-					//						for (int i = 0; i < builderList.size(); i++) {
-					//							point = builderList.getPoint(i);
-					//							point.x = point.x - minPoint.x;
-					//							builderList.setPoint(point, i);
-					//						}
-					//
-					//					if(minPoint.y != 0)
-					//						for (int i = 0; i < builderList.size(); i++) {
-					//							point = builderList.getPoint(i);
-					//							point.y = point.y - minPoint.y;
-					//							builderList.setPoint(point, i);
-					//						}
+			if(newPolly.isDrawingDenied() == true)
+			{
+				helper.x = tempBounds.x;
+				helper.y = tempBounds.y;
+				System.out.println(helper.x);
+				System.out.println(helper.y);
 
 
-					minPoint.x += helper.x;
-					minPoint.y += helper.y;
-					maxPoint.x += helper.x;
-					maxPoint.y += helper.y;
-
-					//					if(minPoint.x > lastPoint.x)
-					//						for (int i = 0; i < builderList.size(); i++) {
-					//							minPoint.x = lastPoint.x;
-					//						}
-					//
-					//					if(minPoint.y > lastPoint.y)
-					//						for (int i = 0; i < builderList.size(); i++) {
-					//							minPoint.y = lastPoint.y;
-					//						}
-
-
-					lastPoint = new Point();
-					lastPoint = builderList.getPoint(builderList.size()-1);
-					final Point firstPoint = builderList.getPoint(0);
-					lastPoint.x -= helper.x;
-					lastPoint.y -= helper.y;
-
-					builderList.removePoint(builderList.size()-1);
-					builderList.addPoint(lastPoint);
-
-					if ( lastPoint.x == firstPoint.x && lastPoint.y == firstPoint.y) {
-
-
-						final Polly polly = new Polly();
-						polly.setList(builderList);
-
-						maxPoint.x += helper.x;
-						maxPoint.y += helper.y;
-
-						System.out.println(bounds);
-						bounds = new Rectangle(minPoint,maxPoint);
-
-						polly.setBounds(bounds);
-						polly.setLayout(bounds);
-						polly.setLabelPosition(new Rectangle(0,0,10,10));
-						if ( canvas.getChildren().size() >= 0)
-							canvas.removeChild(canvas.getChildren().get(canvas.getChildren().size() - 1));
-						canvas.addChild(polly);
-
-					}
-					else {
-						newPolly.setList(builderList);
-
-
-						bounds = new Rectangle(minPoint,maxPoint);
-
-						newPolly.setBounds(bounds);
-						newPolly.setLayout(bounds);
-						newPolly.setLabelPosition(new Rectangle(0,0,10,10));
-
-						if ( canvas.getChildren().size() >= 0)
-							canvas.removeChild(canvas.getChildren().get(canvas.getChildren().size() - 1));
-						canvas.addChild(newPolly);
-					}
-
-
-					builderList = new PointList();
+				for (int i = 0; i < builderList.size()-1; i++) {
+					point = builderList.getPoint(i);
+					point.x = point.x + helper.x;
+					builderList.setPoint(point, i);
 				}
+				for (int i = 0; i < builderList.size()-1; i++) {
+					point = builderList.getPoint(i);
+					point.y = point.y + helper.y;
+					builderList.setPoint(point, i);
+				}
+				minPoint = new Point();
+
+				minPoint.x = helper.x;
+				minPoint.y = helper.y;
 			}
+
+			newPolly.setDrawingDenied(true);
+
+
+			for (int i = 0; i < builderList.size(); i++) {
+				if (minPoint == null)
+					minPoint = builderList.getPoint(i);
+
+				if (maxPoint == null)
+					maxPoint = builderList.getPoint(i);
+
+				point = builderList.getPoint(i);
+
+				if(point.x < minPoint.x)
+					minPoint.x = point.x;
+				if(point.x > maxPoint.x)
+					maxPoint.x = point.x;
+				if(point.y < minPoint.y)
+					minPoint.y = point.y;
+				if(point.y > maxPoint.y)
+					maxPoint.y = point.y;
+			}
+
+			System.out.println(minPoint);
+			System.out.println(maxPoint);
+
+			if(minPoint.x != 0)
+				for (int i = 0; i < builderList.size(); i++) {
+					point = builderList.getPoint(i);
+					point.x = point.x - minPoint.x;
+					builderList.setPoint(point, i);
+				}
+			System.out.println("execute once");
+			if(minPoint.y != 0)
+				for (int i = 0; i < builderList.size(); i++) {
+					point = builderList.getPoint(i);
+					point.y = point.y - minPoint.y;
+					builderList.setPoint(point, i);
+				}
+
+			//
+			//
+			//					minPoint.x += helper.x;
+			//					minPoint.y += helper.y;
+			//					maxPoint.x += helper.x;
+			//					maxPoint.y += helper.y;
+			//
+			//					if(minPoint.x > lastPoint.x)
+			//						for (int i = 0; i < builderList.size(); i++) {
+			//							minPoint.x = lastPoint.x;
+			//						}
+			//
+			//					if(minPoint.y > lastPoint.y)
+			//						for (int i = 0; i < builderList.size(); i++) {
+			//							minPoint.y = lastPoint.y;
+			//						}
+
+
+			lastPoint = new Point();
+			lastPoint = builderList.getPoint(builderList.size()-1);
+			final Point firstPoint = builderList.getPoint(0);
+			//					lastPoint.x -= helper.x;
+			//					lastPoint.y -= helper.y;
+			//
+			//					builderList.removePoint(builderList.size()-1);
+			//					builderList.addPoint(lastPoint);
+
+			if ( (lastPoint.x + 10 >= firstPoint.x && lastPoint.x - 10 <= firstPoint.x) && (lastPoint.y + 10 >= firstPoint.y && lastPoint.y - 10 <= firstPoint.y)) 
+			{
+
+				final Polly polly = new Polly();
+				polly.setList(builderList);
+
+				//				maxPoint.x += helper.x;
+				//				maxPoint.y += helper.y;
+
+
+				bounds = new Rectangle(minPoint,maxPoint);
+				System.out.println(bounds);
+
+				polly.setBounds(bounds);
+				polly.setLayout(bounds);
+				polly.setLabelPosition(new Rectangle(0,0,10,10));
+				if ( canvas.getChildren().size() >= 0)
+					canvas.removeChild(canvas.getChildren().get(canvas.getChildren().size() - 1));
+				canvas.addChild(polly);
+
+			}
+			else {
+				newPolly.setList(builderList);
+
+
+				bounds = new Rectangle(minPoint,maxPoint);
+				System.out.println(bounds);
+
+				newPolly.setBounds(bounds);
+				newPolly.setLayout(bounds);
+				newPolly.setLabelPosition(new Rectangle(0,0,10,10));
+
+				if ( canvas.getChildren().size() >= 0)
+					canvas.removeChild(canvas.getChildren().get(canvas.getChildren().size() - 1));
+				canvas.addChild(newPolly);
+			}
+
+
+			builderList = new PointList();
+		}
 	}
-//			else if (newPolly.isDrawingDenied() == false) {
-//
-//				pollyLine.setBounds(new Rectangle(location, size));
-//				pollyLine.setLayout(new Rectangle(location, size));
-//
-//				final PointList pointList = new PointList(new int[] {0, 0, size.width, size.height});
-//
-//				pollyLine.setList(pointList);
-//				pollyLine.setDrawingDenied(true);
-//
-//				canvas.addChild(pollyLine);
-//			}
-//		} 		
-//		else if (size != null) {
-//
-//			pollyLine.setBounds(new Rectangle(location, size));
-//			pollyLine.setLayout(new Rectangle(location, size));
-//
-//			final PointList pointList = new PointList(new int[] {0, 0, size.width, size.height});
-//
-//			pollyLine.setList(pointList);
-//			pollyLine.setDrawingDenied(true);
-//
-//			canvas.addChild(pollyLine);
-//		}
-	
+	//			else if (newPolly.isDrawingDenied() == false) {
+	//
+	//				pollyLine.setBounds(new Rectangle(location, size));
+	//				pollyLine.setLayout(new Rectangle(location, size));
+	//
+	//				final PointList pointList = new PointList(new int[] {0, 0, size.width, size.height});
+	//
+	//				pollyLine.setList(pointList);
+	//				pollyLine.setDrawingDenied(true);
+	//
+	//				canvas.addChild(pollyLine);
+	//			}
+	//		} 		
+	//		else if (size != null) {
+	//
+	//			pollyLine.setBounds(new Rectangle(location, size));
+	//			pollyLine.setLayout(new Rectangle(location, size));
+	//
+	//			final PointList pointList = new PointList(new int[] {0, 0, size.width, size.height});
+	//
+	//			pollyLine.setList(pointList);
+	//			pollyLine.setDrawingDenied(true);
+	//
+	//			canvas.addChild(pollyLine);
+	//		}
+
 
 
 
