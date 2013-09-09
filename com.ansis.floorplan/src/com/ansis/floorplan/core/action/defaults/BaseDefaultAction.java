874e2fc7f135d9a30d04e6ca9f833faf8d311eff
sbrosteanu <stef.brosteanu@gmail.com>
2013-09-09 09:41:00 +0300

    Added Base Default Action class. WIP

---
+++ com.ansis.floorplan/src/com/ansis/floorplan/core/action/defaults/BaseDefaultAction.java
@@ -0,0 +1,112 @@
+package com.ansis.floorplan.core.action.defaults;
+
+import java.util.HashMap;
+
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.Request;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.ui.actions.SelectionAction;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.ui.IWorkbenchPart;
+
+import com.ansis.floorplan.FloorplanActivator;
+import com.ansis.floorplan.util.color.FPStandardColor;
+import com.ansis.floorplan.util.font.FPFontSize;
+import com.ansis.floorplan.util.font.FPFontStyle;
+
+public abstract class BaseDefaultAction extends SelectionAction {
+
+	//instance fields
+	
+	private final String defaultOpacity = "50"; //$NON-NLS-1$
+	
+	
+	public BaseDefaultAction(IWorkbenchPart part) {
+		super(part);
+	}
+	
+
+	public void prepareSetDefaultFigureColor() 
+	{
+		final Request defaultFigureColorReq = new Request("defaultFigureColor"); //$NON-NLS-1$
+		final HashMap<String, RGB> reqData = new HashMap<String, RGB>();
+		reqData.put("newDefaultFigureColor", getDefaultFigureColor()); //$NON-NLS-1$
+		defaultFigureColorReq.setExtendedData(reqData);
+//		executeAction(defaultFigureColorReq);		
+	}
+
+	
+	public void prepareSetDefaultOpacity() {
+		final Request defaultOpacityReq = new Request("opacity"); //$NON-NLS-1$
+		final HashMap<String, String> reqData = new HashMap<String, String>();
+		reqData.put("newOpacity", getDefaultOpacity()); //$NON-NLS-1$
+		defaultOpacityReq.setExtendedData(reqData);
+	}
+	
+	public void prepareSetDefaulFontColor() {
+		final Request defaultFontColorReq = new Request("defaultFontColor"); //$NON-NLS-1$
+		final HashMap<String, RGB> reqData = new HashMap<String, RGB>();
+		reqData.put("newDefaultFontColor", getDefaultFontColor()); //$NON-NLS-1$
+		defaultFontColorReq.setExtendedData(reqData);
+	}
+	
+	public void prepareSetDefaultFontSize() {
+		final Request defaultFontSizeReq = new Request("fontSize"); //$NON-NLS-1$
+		final HashMap<String, String> reqData = new HashMap<String, String>();
+		reqData.put("newFontSize", getDefaultFontSize()); //$NON-NLS-1$
+		defaultFontSizeReq.setExtendedData(reqData);
+	}
+	
+	public void prepareSetDefaultFontStyle() {
+		final Request defaultFontStyleReq = new Request("fontStyle"); //$NON-NLS-1$
+		final HashMap<String, String> reqData = new HashMap<String, String>();
+		reqData.put("newFontStyle", getDefaultNormal()); //$NON-NLS-1$
+		defaultFontStyleReq.setExtendedData(reqData);
+	}
+	
+	public void prepareSetDefaultLabelColor() {
+		final Request defaultLabelColorReq = new Request("defaultLabelColor"); //$NON-NLS-1$
+		final HashMap<String, RGB> reqData = new HashMap<String, RGB>();
+		reqData.put("newDefaultLabelColor", getDefaultLabelColor()); //$NON-NLS-1$
+		defaultLabelColorReq.setExtendedData(reqData);
+	}
+	
+
+	protected void executeAction(final Request request) {
+		for (Object ob : getSelectedObjects()) {
+
+			final EditPart object = (EditPart)ob;
+			final Command cmd = object.getCommand(request);
+
+			execute(cmd);
+
+		}
+	}
+
+	// ==================== 7. Getters & Setters ====================
+
+	public RGB getDefaultFigureColor() {
+		return FloorplanActivator.getDefault().getColor(FPStandardColor.BLUE_NAVY).getRGB();
+	}
+
+	public String getDefaultOpacity() {
+		return defaultOpacity;
+	}
+	
+	public RGB getDefaultFontColor() {
+		return FloorplanActivator.getDefault().getColor(FPStandardColor.BLACK).getRGB();
+	}
+	
+	public String getDefaultFontSize() {
+		return String.valueOf( FPFontSize.NORMAL.getPercent()/10 );
+	}
+	
+	public String getDefaultNormal() {
+		return String.valueOf( FPFontStyle.NORMAL.getStyle() );
+	}
+	
+	public RGB getDefaultLabelColor() {
+		return FloorplanActivator.getDefault().getColor(FPStandardColor.WHITE).getRGB();
+	}
+	
+}
