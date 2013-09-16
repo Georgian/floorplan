c90b8ca80fbb499dc732bd82b4077369408dbfcc
norbimona <norbi0104@yahoo.com>
2013-09-16 16:11:42 +0300

    Added Base Action class. WIP & Fix ASAP

---
+++ com.ansis.floorplan/src/com/ansis/floorplan/core/action/BaseAction.java
@@ -0,0 +1,69 @@
+package com.ansis.floorplan.core.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.ui.actions.SelectionAction;
+import org.eclipse.ui.IWorkbenchPart;
+
+import com.ansis.floorplan.core.model.CanvasModel;
+import com.ansis.floorplan.core.model.ChildModel;
+
+public abstract class BaseAction extends SelectionAction {
+
+	// ==================== 1. Static Fields ========================
+
+	
+	// ====================== 2. Instance Fields =============================
+
+	private CanvasModel model;
+
+	// ==================== 4. Constructors ====================
+
+	public BaseAction(final IWorkbenchPart part, final CanvasModel model) {
+		super(part);
+		setLazyEnablementCalculation(true);
+		this.model = model;
+	}
+
+	// ==================== 6. Action Methods ====================
+
+	@Override
+	public void run() 
+	{
+		for (final ChildModel children : getSelected()) 
+			changeProperty(children);
+	}
+
+	abstract protected void changeProperty(final ChildModel children);
+
+	@Override
+	protected boolean calculateEnabled() {
+//		System.out.println("!getSelected().isEmpty() : " + !getSelected().isEmpty());
+		return /*!getSelected().isEmpty()*/ true;
+	}
+
+
+	// ==================== 13. Utility Methods ====================
+
+	public List<ChildModel> getSelected()
+	{
+		final List<ChildModel> selectedList = new ArrayList<>();
+
+		for (final Object object : getSelectedObjects())
+			if (object instanceof EditPart)
+				if (((EditPart) object).getModel() instanceof ChildModel)
+					selectedList.add((ChildModel) ((EditPart) object).getModel());
+
+		if (selectedList.isEmpty())
+		{
+			for ( final ChildModel childModel : model.getChildren())
+				selectedList.add(childModel);
+			System.out.println(model.getChildren().size());
+		}
+		
+		return selectedList;
+	}
+
+}
--- com.ansis.floorplan/src/com/ansis/floorplan/core/action/FigureColorAction.java
+++ com.ansis.floorplan/src/com/ansis/floorplan/core/action/FigureColorAction.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
-import org.eclipse.gef.ui.actions.SelectionAction;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.ui.IWorkbenchPart;
 
@@ -16,7 +15,7 @@ import com.ansis.floorplan.core.model.ChildModel;
 import com.ansis.floorplan.util.FPConstPresentation;
 
 
-public class FigureColorAction extends SelectionAction {
+public class FigureColorAction extends BaseAction {
 
 	// ==================== 1. Static Fields ========================
 
@@ -27,41 +26,12 @@ public class FigureColorAction extends SelectionAction {
 
 	private RGB figureColor;
 
-	private final CanvasModel model;
-	
-	private int selection = 0;
-
 
 	// ==================== 4. Constructors ====================
 
 	public FigureColorAction(final IWorkbenchPart part, final CanvasModel model) {
-		super(part);
-		this.model = model;
+		super(part, model);
 		setLazyEnablementCalculation(true);
-	}
-
-
-	// ==================== 5. Creators ====================
-
-	public Command createFigureColorCommand(final RGB figureColor) {
-		final Request figureColorReq = new Request("figureColor"); //$NON-NLS-1$
-		final HashMap<String, RGB> reqData = new HashMap<String, RGB>();
-		reqData.put("newFigureColor", figureColor); //$NON-NLS-1$
-		figureColorReq.setExtendedData(reqData);
-		final EditPart object = (EditPart)getSelectedObjects().get(0);
-		final Command cmd = object.getCommand(figureColorReq);
-		return cmd; 
-	}
-
-
-	// ==================== 6. Action Methods ====================
-
-	@Override
-	protected boolean calculateEnabled() {
-		final Command cmd = createFigureColorCommand(null); 
-		if (cmd == null)
-			return false;
-		return true;
 	}
 
 	@Override
@@ -72,51 +42,39 @@ public class FigureColorAction extends SelectionAction {
 		setEnabled(false);
 	}
 
+
+	// ==================== 6. Action Methods ====================
+
 	@Override
-	public void run() {
+	protected void changeProperty(final ChildModel children) {
 		final Request figureColorReq = new Request("figureColor"); //$NON-NLS-1$
 		final HashMap<String, RGB> reqData = new HashMap<String, RGB>();
 		reqData.put("newFigureColor", getFigureColor()); //$NON-NLS-1$
 		figureColorReq.setExtendedData(reqData);
 
-		if (getSelectedObjects().get(0) != null) {
-			final EditPart object = (EditPart)getSelectedObjects().get(0);
-			final Command cmd = object.getCommand(figureColorReq);
+		final EditPart object = (EditPart)getSelectedObjects().get(0);
+		final Command cmd = object.getCommand(figureColorReq);
 
-			execute(cmd);
+		execute(cmd);		
 
-			final ChildModel firstChild  = (ChildModel) object.getModel();
+		final ChildModel firstChild  = (ChildModel) object.getModel();
 
-			for (final Object ob : getSelectedObjects()) {
+		for (final Object ob : getSelectedObjects()) {
 
-				final EditPart objects = (EditPart)ob;
+			final EditPart objects = (EditPart)ob;
 
-				final ChildModel children = (ChildModel) objects.getModel();
-				children.setColor(firstChild.getColor());
-				
-				if ( children.getFontColorChanged() == false )
-					children.setFontColor(firstChild.getColor());
-			}
-		}
-		else {
-			// This is not yet working
-//			final EditPart object = (EditPart)getSelectedObjects().get(0);
-//			final Command cmd = object.getCommand(figureColorReq);
-//
-//			execute(cmd);
-//
-//			final ChildModel firstChild  = (ChildModel) object.getModel();
-//
-//			for (final ChildModel childModel : model.getChildren())
-//				childModel.setColor(firstChild.getColor());
+			final ChildModel test = (ChildModel) objects.getModel();
+			test.setColor(firstChild.getColor());
+
+			if ( test.getFontColorChanged() == false )
+				test.setFontColor(firstChild.getColor());
 		}
 	}
-
-
+	
+	
 	// ==================== 7. Getters & Setters ====================
 
 	public RGB getFigureColor() {
 		return figureColor;
 	}
-
 }
